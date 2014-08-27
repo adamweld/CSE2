abb578dab9aecaef5bec672f0587b7fe89f44344
Adam Weld <adam.weld@gmail.com>
2014-08-27 12:41:11 -0400

    program commit

---
+++ lab01-HelloWorld/HelloWorld.java
@@ -0,0 +1,19 @@
+//Adam Weld
+//Hello World Program
+//
+//javac HelloWorld.java
+//java HelloWorld
+
+//define first class
+public class HelloWorld{
+
+	//create main method
+	public static void main(String[] args) {
+		
+		//print hello world message
+		System.out.println("Hello World");
+		System.out.println("CSE2 class program");
+		System.out.println("Adam Weld");
+		System.out.println("text test string");
+	}
+}
