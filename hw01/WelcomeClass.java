db59f903ac4cbea6e2c6df0be91225b3d4024ed7
Adam Weld <adam.weld@gmail.com>
2014-08-27 17:54:35 -0400

    not quite fully working welcome message

---
+++ hw01/WelcomeClass.java
@@ -0,0 +1,21 @@
+//WelcomeClass.java
+//Displays welcome text and ascii art
+//
+//Adam Weld
+//CSE2
+
+//define program class
+public class WelcomeClass{
+	//add main method
+	public static void main(String[] args) {
+		//print welcome message
+		System.out.println("    -----------     ");
+		System.out.println("    | WELCOME |     ");
+		System.out.println("    -----------     ");
+		System.out.println("  ^  ^  ^  ^  ^  ^  ");
+//		System.out.println(" / \/ \/ \/ \/ \/ \ ");
+		System.out.println("|-A--A--W--2--1--8-|");
+//		System.out.println(" \ /\ /\ /\ /\ /\ / ");
+		System.out.println("  v  v  v  v  v  v  ");
+	}
+}
