9bfcefa85623aea9f2b9b485f4d782feb12a6ca8
Adam Weld <adam.weld@gmail.com>
2014-11-14 13:18:41 -0500

    updates

--- hw10/PokerOdds.java
+++ hw10/PokerOdds.java
@@ -8,42 +8,124 @@ import java.util.Random;
 import java.util.Scanner;
 public class PokerOdds{
 
+  public static String cardFormat(int x) {
+    switch (x) {
+      case (0) :
+        return "A ";
+      case (1) :
+        return "K ";
+      case (2) :
+        return "Q ";
+      case (3) :
+        return "J ";
+      case (4) :
+        return "10 ";
+      case (5) :
+        return "9 ";
+      case (6) :
+        return "8 ";
+      case (7) :
+        return "7 ";
+      case (8) :
+        return "6 ";
+      case (9) :
+        return "5 ";
+      case (10) :
+        return "4 ";
+      case (11) :
+        return "3 ";
+      case (12) :
+        return "2 ";
+      default:
+        return "err";
+    }
+  }
+
   public static void showHands() {
     // cards can be [0,51]
     // Clubs [0,12], Diamonds [13,25]
     // then Hearts, Spades
+    Scanner scan=new Scanner(System.in); // create scanner object
     Random rand = new Random(); // create random object
-    int[] deck = new int[52];
-    for(int y = 0; y < 52; y++) { // initialize deck with [0,51]
-      deck[y] = y;
-    }
-    int deckMax = 52;
-    int[] hand = {-1, -1, -1, -1, -1};
+    String answer=""; // will be called later if to check if user wants to repeat
+    do {
+      int[] deck = new int[52];
+      for(int y = 0; y < 52; y++) { // initialize deck with [0,51]
+        deck[y] = y;
+      }
+      int deckMax = 52; // the largest 'real' value in deck array
+                        // note that index will be (deckMax - 1)
+      int[] hand = {-1, -1, -1, -1, -1};
 
-    for(int i = 0; i < 5; i++) {
-      int n = rand.nextInt(deckMax);
-      hand[i] = deck[n];
-      deck[n] = deck[deckMax-1];
-      deck[deckMax-1] = -1;
-      deckMax--;
-      System.out.println(FindDuplicates.listArray(hand));
-    }
+      for(int i = 0; i < 5; i++) {
+        int n = rand.nextInt(deckMax); // get random value less than deckMax
+        hand[i] = deck[n]; // take card into hand
+        deck[n] = deck[deckMax-1]; // replace card that was taken with last card in deck
+        deck[deckMax-1] = -1; // take a card out of the deck
+        deckMax--; // deck now has one less card
+        System.out.println(FindDuplicates.listArray(hand));
+      }
 
 
-    String labels[] = {"Clubs: ","Diamonds: ","Hearts: ","Spades: "};
-    //for(int p = 0; p < 4; p++) {
-    //  System.out.println(labels[p]+cards[p]);
-    //}
+      // array of the values of each card, sorted by suit
+      // e.g. A Q 3
+      String[] cards = {"", "", "", "", ""}; // clubs, diamonds, hearts, spades
+      for(int j = 0; j < 4; j++) {
+        for(int f = 0; f < 5; f++) { // check each value of the hand
+          if(j == hand[f] / 13) { // if in suit (0,1,2,3,4)
+            cards[j] += cardFormat(hand[f] % 13); // get the card number/letter from rank
+            System.out.println("rank: " + hand[f] + " suit: " + j + " value: "
+                + hand[f] % 13 + " name: " + cardFormat(hand[f] % 13));
+          }
+        }
+      }
 
+
+
+      String labels[] = {"Clubs: ","Diamonds: ","Hearts: ","Spades: "};
+      for(int p = 0; p < 4; p++) {
+        System.out.println(labels[p]+cards[p]);
+      }
+
+      System.out.print("Go again? Enter 'y' or 'Y', anything else to quit- ");
+      answer=scan.next();
+
+    } while(answer.equals("Y") || answer.equals("y"));
   }
-
   public static void simulateOdds() {
+    Random rand = new Random(); // create random object
+    int[] numExactlyOnePair = new int[13];
+    int numNotOnePair = 0;
+    for(int p = 0; p < 10000; p++) { // run 10,000 trials
+      int[] deck = new int[52];
+      for(int y = 0; y < 52; y++) { // initialize deck with [0,51]
+        deck[y] = y;
+      }
+      int deckMax = 52; // the largest 'real' value in deck array
+                        // note that index will be (deckMax - 1)
+      int[] hand = {-1, -1, -1, -1, -1};
 
+      for(int i = 0; i < 5; i++) {
+        int n = rand.nextInt(deckMax); // get random value less than deckMax
+        hand[i] = deck[n]; // take card into hand
+        deck[n] = deck[deckMax-1]; // replace card that was taken with last card in deck
+        deck[deckMax-1] = -1; // take a card out of the deck
+        deckMax--; // deck now has one less card
+      }
+    }
+    String[] labels = {"A", "K", "Q", "J", "10", "9", "8", "7", "6", "5", "4", "3", "2"};
+    System.out.println(" rank  freq of exactly one pair");
+    for(int i = 0; i < 13; i++) {
+      System.out.printf("%s    %4d \n",labels[i],numExactlyOnePair[i]);
+    }
+    System.out.println("------------------------------------------");
+    System.out.println("total not exactly one pair: " + numNotOnePair);
+    
   }
 
   public static void main(String [] arg){
     showHands();
-    //simulateOdds();
+    simulateOdds();
   }
 }
 
